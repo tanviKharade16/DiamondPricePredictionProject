182bd3a8aa31c65eefd94428540b15afb4a87964
tanviKharade16 <tanvikharade16@gmail.com>
2025-04-06 09:13:47 +0530

    Model trainer commit

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 from src.exception import CustomException
 from src.logger import logging
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 
 def save_object(file_path, obj):
     try:
@@ -19,4 +20,29 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            # Train model
+            model.fit(X_train,y_train)
+
+            
+
+            # Predict Testing data
+            y_test_pred =model.predict(X_test)
+
+            # Get R2 scores for train and test data
+            #train_model_score = r2_score(ytrain,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] =  test_model_score
+
+        return report
+
+    except Exception as e:
+        logging.info('Exception occured during model training')
+        raise CustomException(e,sys)
     
