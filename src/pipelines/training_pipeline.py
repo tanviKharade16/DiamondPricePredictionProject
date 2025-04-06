56739a071f631b86ec576e3b8d571febc4e19752
tanviKharade16 <tanvikharade16@gmail.com>
2025-04-06 09:12:31 +0530

    Model trainer commit

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 if __name__ == "__main__":
     obj = DataIngestion()
@@ -16,6 +17,7 @@ if __name__ == "__main__":
     data_transformation = DataTransformation()
     train_arr,test_arr,_=data_transformation.initaite_data_transformation(train_data_path,test_data_path)
     
-    
+    model_trainer=ModelTrainer()
+    model_trainer.initate_model_training(train_arr,test_arr)
      
    
